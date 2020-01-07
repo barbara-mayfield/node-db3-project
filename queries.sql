af517de8923f9e2c39b98fcae2a9e8bf84d55501
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-07 05:51:32 -0600

    3rd SQL query completed

--- queries.sql
+++ queries.sql
@@ -13,4 +13,9 @@ WHERE o.ShippedDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p.ProductName, od.Quantity
+FROM OrderDetail AS od 
+JOIN Product AS p on od.ProductId = p.Id
+WHERE od.OrderId = 10252;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
