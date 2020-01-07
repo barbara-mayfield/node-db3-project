2e9599c9476e7bb82bb0810926d2de9568fdf67b
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-07 09:00:01 -0600

    scheme-model.js findSteps working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,8 +10,11 @@ function findById(id) {
         .first()
 }
 
-function findSteps() {
-    
+function findSteps(id) {
+    return db("schemes as sc") 
+        .join("steps as st", "sc.id", "st.scheme_id")
+        .where({ scheme_id: id })
+        .select("st.id", "sc.scheme_name", "st.step_number", "st.instructions")
 }
 
 function add() {
