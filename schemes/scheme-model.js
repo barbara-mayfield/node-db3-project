39b5d71431c24a95aae28f68e85012ce9aed61f6
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-07 06:04:40 -0600

    Scheme-model.js framed out

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,34 @@
 const db = require("../data/dbConfig.js")
+
+function find() {
+
+}
+
+function findById() {
+    
+}
+
+function findSteps() {
+    
+}
+
+function add() {
+    
+}
+
+function update() {
+    
+}
+
+function remove() {
+    
+}
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
